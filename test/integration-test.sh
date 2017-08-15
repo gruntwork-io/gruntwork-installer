90df1faac6b63d45b50a35ff9e8d8ccd0a764e03
Yevgeniy Brikman <brikis98@gmail.com>
2017-08-16 01:30:39 +0200

    Update test to check module-params

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -9,12 +9,6 @@ readonly LOCAL_INSTALL_URL="file:///src/gruntwork-install"
 echo "Using local copy of bootstrap installer to install local copy of gruntwork-install"
 ./src/bootstrap-gruntwork-installer.sh --download-url "$LOCAL_INSTALL_URL" --version "ignored-for-local-install"
 
-echo "Using gruntwork-install to install a few modules from script-modules"
-gruntwork-install --module-name "vault-ssh-helper" --repo "https://github.com/gruntwork-io/script-modules" --tag "~>0.0.21"
-
-echo "Checking that the vault-ssh-helper installed correctly"
-/etc/user-data/vault-ssh-helper/download-ca-cert.sh --help
-
 echo "Using gruntwork-install to install a module from the module-ecs repo"
 gruntwork-install --module-name "ecs-scripts" --repo "https://github.com/gruntwork-io/module-ecs" --branch "v0.0.1"
 
@@ -23,6 +17,9 @@ gruntwork-install --module-name "ecs-scripts" --repo "https://github.com/gruntwo
 
 echo "Checking that the ecs-scripts installed correctly"
 configure-ecs-instance --help
+
+echo "Using gruntwork-install to install a module from the gruntwork-install repo and passing args to it via --module-param"
+gruntwork-install --module-name "dummy-module" --repo "https://github.com/gruntwork-io/gruntwork-installer" --branch "fix-args" --module-param "file-to-cat=README.md"
 
 echo "Using gruntwork-install to install a binary from the gruntkms repo"
 gruntwork-install --binary-name "gruntkms" --repo "https://github.com/gruntwork-io/gruntkms" --tag "v0.0.1"
