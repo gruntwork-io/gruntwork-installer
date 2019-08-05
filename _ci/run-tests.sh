3257b43828f2ba138a820583db45feeee688cc70
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-08-05 15:50:26 -0700

    Add ubuntu18 to tests

--- _ci/run-tests.sh
+++ _ci/run-tests.sh
@@ -6,10 +6,12 @@ cd "$SCRIPT_DIR/.."
 
 echo "Building docker containers for test"
 docker build -t gruntwork/gruntwork-installer-ubuntu test/ubuntu
+docker build -t gruntwork/gruntwork-installer-ubuntu18 test/ubuntu18
 docker build -t gruntwork/gruntwork-installer-amazonlinux test/amazonlinux
 docker build -t gruntwork/gruntwork-installer-centos test/centos
 
 echo "Running integration tests using docker-compose"
 docker-compose -f test/ubuntu/docker-compose.yml run installer /test/integration-test.sh
+docker-compose -f test/ubuntu18/docker-compose.yml run installer /test/integration-test.sh
 docker-compose -f test/amazonlinux/docker-compose.yml run installer /test/integration-test.sh
 docker-compose -f test/centos/docker-compose.yml run installer /test/integration-test.sh
